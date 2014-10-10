2f3ca2936a50890ca44d0ee60a70f918b57ac8ff
EricaJoy <github@ericabaker.com>
2014-10-10 12:44:07 -0700

    moved url testing into a function in prep for async implementation

--- bookmarkcleanup.js
+++ bookmarkcleanup.js
@@ -111,17 +111,44 @@ function treeWalk(obj) {
     if (obj.children) {
         if (obj.title.length > 0){
             $("#bookmarks").append('<tr class="info" id="'+obj.id+'"><td colspan="3"> <b>'+obj.title+'</b></td></tr>');}
+            
+            if (typeof bookmarksArray === "undefined") {
+                // Make an empty array to hold the bookmarks
+                bookmarksArray = []
+            }
+            else {
+                //Do some stuff with the current bookmarksArray
+                console.log (bookmarksArray)
+                    for (var i=0; i < bookmarksArray.length; i++) {
+                        urlTesting(bookmarksArray[i]);
+                    }
+                // Empty out the bookmarksArray array
+                bookmarksArray = []
+            }
+            
             // for each child, do the tree walk
             for (var i=0; i < obj.children.length; i++) {
                 treeWalk(obj.children[i]);
+                // console.log(urls)
 
             }
 
         
     }
     if (obj['url']) {
+        // Test to make sure its not a "special" bookmark.
         if (obj.id && (obj.url.indexOf('javascript:') < 0) && (obj.url.indexOf('data:') < 0) && (obj.url.indexOf('about:') < 0)) {
-        
+        // Beginning the code for async
+        bookmarksArray.push(obj)
+ 
+            }
+        }
+
+    }
+
+
+function urlTesting(obj) {
+
         $.ajax({
             url: obj.url,
             type: 'GET',
@@ -167,11 +194,7 @@ function treeWalk(obj) {
           }
 
         });
- 
-            }
-        }
-
-    }
+}
 
 
 
